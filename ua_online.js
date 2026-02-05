605694903f72d2cabd4d6bc416a74bb21b204985
Антон <mikolenko.anton1@gmail.com>
2026-02-05 19:32:35 +0200

    Update ua_online.js

--- ua_online.js
+++ ua_online.js
@@ -1,75 +1,73 @@
 (function() {
     'use strict';
-    
-    // Іконка "HOT" (SVG текст)
-    var hot_icon = '<svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="100" height="100" rx="20" fill="#FF4757"/><text x="50" y="65" font-family="Arial, sans-serif" font-size="45" font-weight="bold" fill="white" text-anchor="middle">HOT</text></svg>';
 
-    var ua_component = {
-        title: 'UA Online',
-        component: 'uaonline',
-        icon: hot_icon,
-        on: function() {
-            var items = [
-                {t:'UAKino', u:'https://uakino.cx/'}, 
-                {t:'UAFLIX', u:'https://uafix.net/'},
-                {t:'Kinoukr', u:'https://kinoukr.tv/'},
-                {t:'UASerials', u:'https://uaserials.com/'}
-            ];
-            
-            var html = Lampa.List(items.map(u => ({
-                title: u.t,
-                url: u.u,
-                component: 'ua_search'
-            })), {
-                on_select: (item) => {
-                    Lampa.Input.edit({
-                        title: 'Пошук ' + item.title,
-                        value: '',
-                        free: true,
-                        nosave: true
-                    }, (query) => {
-                        Lampa.Activity.push({
-                            url: item.url,
-                            title: 'Пошук: ' + query,
-                            component: 'browser',
-                            page: 1
-                        });
-                        Lampa.Browser.open({ url: item.url + '?s=' + encodeURIComponent(query) });
+    // Іконка HOT
+    var hot_icon = '<svg viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="100" height="100" rx="20" fill="#FF4757"/><text x="50" y="65" font-family="Arial" font-size="40" font-weight="bold" fill="white" text-anchor="middle">HOT</text></svg>';
+
+    // Функція UA Online
+    function uaMenu() {
+        var items = [
+            {title: '🔥 UAKino', url: 'https://uakino.cx/?s='},
+            {title: '📱 UAFLIX', url: 'https://uafix.net/?s='},
+            {title: '🎬 Kinoukr', url: 'https://kinoukr.tv/search/?story='},
+            {title: '📺 UASerials', url: 'https://uaserials.com/?s='}
+        ];
+
+        Lampa.Select.show({
+            title: 'UA Online - Вибери сайт',
+            items: items,
+            onSelect: (item) => {
+                Lampa.Input.edit({
+                    title: item.title + ' пошук',
+                    value: '',
+                    free: true
+                }, (query) => {
+                    Lampa.Browser.open({
+                        url: item.url + encodeURIComponent(query),
+                        title: item.title + ': ' + query
                     });
-                }
-            });
-            Lampa.Controller.content(html);
+                });
+            }
+        });
+    }
+
+    // MSX меню селектори
+    function addMSXMenu() {
+        // Перевіряємо різні MSX меню
+        var selectors = [
+            '.menu .menu__list .menu__item',
+            '.selector-menu .menu__item',
+            '.menu-category .menu__item',
+            '.view--menu .menu__item'
+        ];
+
+        var added = false;
+        selectors.forEach(sel => {
+            if ($(sel).length && !$(sel).find('[data-ua-hot]').length) {
+                var item = $(`<div class="menu__item selector" data-ua-hot="1">
+                    <div class="menu__ico" style="width:32px;height:32px;margin-right:10px;">${hot_icon}</div>
+                    <div class="menu__text">UA Online</div>
+                </div>`);
+
+                item.on('hover:enter', uaMenu);
+                $(sel).last().after(item);
+                added = true;
+                Lampa.Noty.show('🔥 UA Online додано в меню!');
+            }
+        });
+
+        if (!added) {
+            Lampa.Noty.show('MSX меню знайдено, але селектор інший. Дебаг...');
         }
-    };
-
-    // Додаємо в меню з назвою "UA Online" та іконкою "HOT"
-    function addMenu() {
-        if ($('.menu .menu__item[data-action=uaonline]').length) return;
-        
-        var item = $(`<div class="menu__item selector" data-action="uaonline">
-            <div class="menu__ico" style="width:30px; height:30px;">${hot_icon}</div>
-            <div class="menu__text">UA Online</div>
-        </div>`);
-
-        item.on('hover:enter', function() {
-            Lampa.Activity.push({
-                url: '',
-                title: 'UA Online',
-                component: 'uaonline',
-                page: 1
-            });
-        });
-
-        // Вставляємо ПІСЛЯ "Пошук" (індекс 1 або 2)
-        $('.menu .menu__list').eq(0).find('.menu__item').eq(1).after(item);
     }
 
-    if (window.appready) addMenu();
-    else {
-        Lampa.Listener.follow('app', function(e) {
-            if (e.type == 'ready') addMenu();
-        });
-    }
+    // Запуск для MSX
+    setTimeout(addMSXMenu, 3000);
     
-    Lampa.Component.add('uaonline', ua_component);
+    // Listener для повного завантаження
+    Lampa.Listener.follow('app', (e) => {
+        if (e.type == 'ready') setTimeout(addMSXMenu, 2000);
+    });
+
+    Lampa.Noty.show('🚀 UA плагін для MSX запущено!');
 })();
