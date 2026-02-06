ded15a06b9cf4df7b6f2e2479a2dfa7156234ab0
Антон <mikolenko.anton1@gmail.com>
2026-02-06 20:17:20 +0200

    Update ua_online.js

--- ua_online.js
+++ ua_online.js
@@ -1,87 +1,49 @@
 (function() {
     'use strict';
 
-    const NAME = 'UA + Online FREE';
-    const SOURCES = [
-        {title: 'UAKino CX', url: 'https://uakino.cx/?s=QUERY'},
-        {title: 'UAFLIX', url: 'https://uafix.net/?s=QUERY'},
-        {title: 'UASerials', url: 'https://uaserials.com/?s=QUERY'},
-        {title: 'Takflix UA', url: 'https://takflix.com/uk/search?q=QUERY'},
-        {title: 'Lumex HD', action: () => Lampa.Source.get('lumex')},
-        {title: 'RedHead HD', action: () => Lampa.Source.get('redhead')},
-        {title: 'HDrezka Free', action: () => Lampa.Source.get('hdrezka')},
-        {title: 'Fan Serials', action: () => Lampa.Source.get('fanserials')}
-    ];
+    var ua_component = {
+        search: function(object) {
+            var title = object.movie.title + ' ' + (object.movie.original_title || '');
+            var html = '<div class="full-start__item selector focus" style="background: linear-gradient(45deg,#ff4757,#ff6b7a);color:#fff"><div class="full-start__title">🔥 UA FREE HD</div></div>';
+            $('.full-start__buttons, .actions__line').append(html);
+            
+            $(document).on('hover:enter','.full-start__item', function(e){
+                var items = [
+                    {title:'UAKino', url:'https://uakino.cx/?s='+encodeURIComponent(title)},
+                    {title:'UAFLIX', url:'https://uafix.net/?s='+encodeURIComponent(title)},
+                    {title:'UASerials', url:'https://uaserials.com/?s='+encodeURIComponent(title)},
+                    {title:'Takflix', url:'https://takflix.com/uk/search?q='+encodeURIComponent(title)},
+                    {title:'Lumex HD', action: function(){Lampa.Source.get('lumex')}},
+                    {title:'RedHead HD', action: function(){Lampa.Source.get('redhead')}}
+                ];
+                
+                Lampa.Select.show({
+                    title: '6 HD джерел',
+                    items: items.map(function(item){
+                        return {
+                            title: item.title,
+                            action: function(){
+                                if(item.action) item.action();
+                                else Lampa.Browser.open({url:item.url,title:item.title});
+                            }
+                        }
+                    }),
+                    onBack: function(){Lampa.Controller.toggle('full')}
+                });
+            });
+        }
+    };
 
-    // Фікс для MSX: listener на playlist + multiple containers
-    Lampa.Listener.follow('playlist', function(e) {
-        setTimeout(addButton, 500);
-    });
     Lampa.Listener.follow('full', function(e) {
-        if (e.type == 'complite') setTimeout(addButton, 2000);
-    });
-
-    function addButton() {
-        var title = $('.info__title').text() || 
-                    $('.view--category_full .full-info__title').text() || 
-                    $('h1').text() || '';
-        if (!title) return;
-
-        $('.ua-free-btn').remove();
-
-        var btn = Lampa.Template.get('button_item', {
-            title: NAME,
-            icon: '🔥',
-            href: ''
-        });
-        btn.addClass('ua-free-btn selector');
-
-        btn.on('hover:enter', () => showMenu(title));
-
-        // 5 місць додавання для MSX/Hisense
-        const places = [
-            '.full-start__buttons',
-            '.full-startbuttons', 
-            '.actions__body',
-            '.view--playlist .actions',
-            '.full-actions'
-        ];
-        
-        places.forEach(selector => {
-            $(selector).append(btn.clone(true));
-        });
-
-        Lampa.Noty.show('🔥 ' + NAME + ' (' + SOURCES.length + ') для ' + title);
-    }
-
-    function showMenu(title) {
-        const items = SOURCES.map(s => ({
-            title: s.title,
-            action: () => {
-                if (s.action) s.action();
-                else {
-                    const query = encodeURIComponent(title);
-                    Lampa.Browser.open({
-                        url: s.url.replace('QUERY', query),
-                        title: s.title
-                    });
+        if (e.type == 'complite') {
+            setTimeout(function(){
+                if ($('.view--category_full').length) {
+                    ua_component.search(e.data);
+                    Lampa.Noty.show('🔥 UA FREE HD додано!');
                 }
-                Lampa.Controller.toggle('full');
-            }
-        }));
-
-        Lampa.Select.show({
-            title: 'HD Джерела',
-            items,
-            onBack: () => Lampa.Controller.toggle('full')
-        });
-    }
-
-    // Меню в головному
-    Lampa.Component.add('uafree', {
-        name: NAME,
-        url: 'uafree'
+            }, 1500);
+        }
     });
 
-    Lampa.Noty.show('🔥 ' + NAME + ' активовано!');
+    Lampa.Noty.show('🔥 UA плагін активовано!');
 })();
